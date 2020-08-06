7cf98f09d77ec5c14c6b118e43f5bbfb15cf9b93
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-08-06 08:16:22 -0400

    gah windows

--- scripts/win_ci.bat
+++ scripts/win_ci.bat
@@ -2,8 +2,8 @@
 :: this is a nasty hack, and should not be needed, but the env isn't _quite_ right
 call deactivate
 call C:\Miniconda\envs\jupyterlab-drawio\Scripts\activate
-call doit %%*
-call doit %%* || goto :error
+call python -m doit %%*
+call python -m doit %%* || goto :error
 
 goto :EOF
 
