8d408f55b43b0ed5b4060119b9458b53ca13b59f
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-08-07 09:00:13 -0400

    work on roadmap while debugging

--- scripts/win_ci.bat
+++ scripts/win_ci.bat
@@ -2,7 +2,8 @@
 :: this is a nasty hack, and should not be needed, but the env isn't _quite_ right
 call conda info
 call deactivate
-call C:\Miniconda\envs\jupyterlab-drawio\Scripts\activate
+call C:\Miniconda\envs\jupyterlab-drawio\Scripts\activate.bat
+call conda info
 call python -m doit %%*
 call python -m doit %%* || goto :error
 
