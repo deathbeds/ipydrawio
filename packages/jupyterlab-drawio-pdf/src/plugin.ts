41fd596a9f58c9c2da20638c3c7ce2b6a60f1626
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-08-09 21:12:23 -0400

    license up

--- packages/jupyterlab-drawio-pdf/src/plugin.ts
+++ packages/jupyterlab-drawio-pdf/src/plugin.ts
@@ -1,4 +1,4 @@
-// Copyright 2020 Dead Pixels Collective
+// Copyright 2020 jupyterlab-drawio contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 import { JupyterLab, JupyterFrontEndPlugin } from '@jupyterlab/application';
 
 import { IDiagramManager } from '@deathbeds/jupyterlab-drawio/lib/tokens';
